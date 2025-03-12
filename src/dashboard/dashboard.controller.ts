9e31ee7a2cc59017ac2772cbb9fbdc0fc54cdbdc
leonardo <nickholasleonardo@gmail.com>
2025-03-12 11:12:19 +0700

    feat: dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('/api/dashboard')
@@ -11,6 +11,21 @@ export class DashboardController {
   async getReport(): Promise<any> {
     return this.marketplaceClient
       .send({ module: 'dashboard', action: 'getReportData' }, {})
+      .toPromise();
+  }
+
+  @Get('/store')
+  async getStoreReport(@Query('store_id') storeId: string): Promise<any> {
+    if (!storeId) {
+      return {
+        success: false,
+        message: 'store_id is required',
+        statusCode: 400,
+      };
+    }
+
+    return this.marketplaceClient
+      .send({ module: 'storeReport', action: 'getStoreReport' }, storeId)
       .toPromise();
   }
 }
