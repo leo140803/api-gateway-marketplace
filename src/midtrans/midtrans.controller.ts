c44a6f39ba09597f7f9fb3b041cd327c988560bd
leonardo <nickholasleonardo@gmail.com>
2025-04-28 00:57:07 +0700

    feat: tripay

--- src/midtrans/midtrans.controller.ts
+++ src/midtrans/midtrans.controller.ts
@@ -6,6 +6,9 @@ import {
   Get,
   HttpException,
   Inject,
+  Post,
+  Headers,
+  Body,
 } from '@nestjs/common';
 import { Response } from 'express';
 import {
@@ -14,6 +17,7 @@ import {
   Transport,
 } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import * as crypto from 'crypto';
 
 @Controller('api/midtrans')
 export class MidtransController {
@@ -22,47 +26,61 @@ export class MidtransController {
   ) {}
 
   //
-  @Get('notification')
+  @Post('notification')
   @HttpCode(200)
-  async handleGetNotification(@Query() query: any, @Res() res: Response) {
+  async handleTripayNotification(
+    @Headers('x-callback-signature') callbackSignature: string,
+    @Headers('x-callback-event') callbackEvent: string,
+    @Body() body: any,
+    @Res() res: Response,
+  ) {
     try {
-      console.log('Notification received from Midtrans (GET):', query);
-      const result = await firstValueFrom(
-        this.transactionClient.send(
-          {
-            module: 'transaction',
-            action: 'notificationMidtrans',
-          },
-          query,
-        ),
-      );
-      if (result.success) {
-        return res.redirect(result.redirectUrl);
+      console.log('Tripay Notification Received:', JSON.stringify(body));
+
+      const privateKey = 'NV5zw-d5a2P-7WeT0-6D1ij-jSPxj'; // Tripay Private Key
+      const jsonString = JSON.stringify(body);
+
+      const expectedSignature = crypto
+        .createHmac('sha256', privateKey)
+        .update(jsonString)
+        .digest('hex');
+
+      // 1. Validate Signature
+      if (callbackSignature !== expectedSignature) {
+        console.error('Invalid Signature from Tripay!');
+        return res
+          .status(400)
+          .json({ success: false, message: 'Invalid signature' });
       }
 
-      // Forward request to microservice
-      // const result = await firstValueFrom(
-      //   this.midtransServiceClient.send(
-      //     { module: 'midtrans', action: 'handleNotification' },
-      //     query,
-      //   ),
-      // );
+      // 2. Validate Event Type
+      if (callbackEvent !== 'payment_status') {
+        console.error('Unknown Callback Event:', callbackEvent);
+        return res
+          .status(400)
+          .json({ success: false, message: 'Invalid callback event' });
+      }
 
-      // // Handle response from microservice
-      // if (result.success) {
-      //   return res.redirect(result.redirectUrl);
-      // } else {
-      //   throw new HttpException(
-      //     result.message || 'Internal Server Error',
-      //     result.statusCode || 500,
-      //   );
-      // }
+      console.log('NOTIFICATION: ' + body);
+
+      // 3. Process Notification
+      const result = await this.transactionClient
+        .send({ module: 'transaction', action: 'notificationTripay' }, body)
+        .toPromise();
+
+      if (result.success) {
+        return res.json({ success: true });
+      } else {
+        return res
+          .status(400)
+          .json({ success: false, message: result.message });
+      }
     } catch (error) {
-      console.error(
-        'Error handling Midtrans notification in API Gateway:',
-        error,
-      );
-      res.status(500).send('Internal Server Error');
+      console.log(error);
+      console.error('Error Handling Tripay Notification:', error.message);
+      return res
+        .status(500)
+        .json({ success: false, message: 'Internal Server Error' });
     }
   }
 }
